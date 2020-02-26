aa031af6f509d3fd5c743223064f595dcefcab7f
ryan6015 <hello_white@163.com>
2020-02-26 22:03:01 +0800

    添加校验方法

--- miniprogram/pages/addpage/addpage.js
+++ miniprogram/pages/addpage/addpage.js
@@ -1,53 +1,66 @@
 // miniprogram/pages/addpage/addpage.js
-Page({
+const validateJs = require('./validate.js')
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
     formData: {
-      hospitalName: ''
+      // 医院名称
+      hospitalName: '',
+      // 科室
+      department: '',
+      // 品牌名称是否是Olympus
+      isOlympus: null,
+      // Olympus品牌型号
+      olympusModel: [],
+      // 腔镜数量
+      laparNum: null,
+      // 品牌名称
+      brand: '',
+      // 型号
+      model: '',
+      // 购买年份
+      year: '',
+      // 科室床位数
+      bedNum: null,
+      // 科室收入
+      income: null,
+      // 科室总手术量
+      operationNum: null,
+      // 腔镜手术占比
+      percent: null
     },
     isOlympusOptions: ['Olympus', '其它'],
-    isOlympus: null,
-    olympusModel: [],
+    oneButton: [{ text: '确定' }],
+    dialogShow: false,
     olympusModelOptions: [
-      { 'name': 'OTV--S400', 'checked': false},
-      { 'name': 'OTV-S300', 'checked': false},
-      { 'name': 'OTV-S190', 'checked': false},
-      { 'name': 'CV-190', 'checked': false},
-      { 'name': '3D', 'checked': false},
-      { 'name': 'OTV-S7PRO', 'checked': false},
-      { 'name': 'OTV-S7', 'checked': false},
-      { 'name': 'CV-170', 'checked': false}
+      { 'gid': 0, 'name': 'OTV--S400', 'checked': false},
+      { 'gid': 1, 'name': 'OTV-S300', 'checked': false},
+      { 'gid': 2, 'name': 'OTV-S190', 'checked': false},
+      { 'gid': 3, 'name': 'CV-190', 'checked': false},
+      { 'gid': 4, 'name': '3D', 'checked': false},
+      { 'gid': 5, 'name': 'OTV-S7PRO', 'checked': false},
+      { 'gid': 6, 'name': 'OTV-S7', 'checked': false},
+      { 'gid': 7, 'name': 'CV-170', 'checked': false}
     ]
   },
   submitForm: function () {
     let that = this
-    this.selectComponent('#form').validate((valid, errors) => {
-      console.log('valid', valid, errors)
-      if (!valid) {
-        wx.showToast({
-          title: errors[0]['message']
-        })
-      } else {
-        wx.cloud.callFunction({
-          name: 'add',
-          data: {
-            'hospitalName': that.data.formData.hospitalName
-          }
-        }).then(res => {
-          wx.showToast({
-            title: 'success'
-          })
-        }).catch(() => {
-          wx.showToast({
-            title: 'fail',
-            type: 'warn'
-          })
-        })
-      }
-    })
+    console.log(this.data.formData)
+    if (validateJs.validate(this.data.formData)) {
+      this.setData({
+        formDataText: JSON.stringify(this.data.formData),
+        dialogShow: true
+      })
+    }
+    // wx.cloud.callFunction({
+    //   name: 'add',
+    //   data: {
+    //     'hospitalName': that.data.formData.hospitalName
+    //   }
+    // })
   },
   formInputChange (e) {
     const { field } = e.currentTarget.dataset
@@ -56,18 +69,44 @@ Page({
     })
   },
   bindIsOlympusChange: function (e) {
+    let value = e.detail.value
+    let brand = 'Olympus'
+    // 如果品牌名称选择其它
+    if (value === '1') {
+      brand = ''
+      let checkboxItems = this.data.olympusModelOptions
+      // 清空之前的选择
+      let newCheckboxItems = checkboxItems.map(item => {
+        item.checked = false
+        return item
+      })
+      this.setData({
+        ['formData.olympusModel']: [],
+        olympusModelOptions: newCheckboxItems
+      })
+    } else {
+      this.setData({
+        ['formData.model']: '',
+        ['formData.year']: '',
+        ['formData.bedNum']: '',
+        ['formData.income']: '',
+        ['formData.operationNum']: '',
+        ['formData.percent']: ''
+      })
+    }
     this.setData({
-      isOlympus: e.detail.value
+      [`formData.isOlympus`]: e.detail.value,
+      [`formData.brand`]: brand
     })
   },
   bindOlympusModelChange: function (e) {
-    var checkboxItems = this.data.olympusModelOptions, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
+    var checkboxItems = this.data.olympusModelOptions, values = e.detail.value
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; i++) {
+      checkboxItems[i].checked = false
+      for (var j = 0, lenJ = values.length; j < lenJ; j++) {
+        if (checkboxItems[i].name == values[j]) {
+          checkboxItems[i].checked = true
+          break
         }
       }
     }
@@ -78,8 +117,12 @@ Page({
   },
   bindYearChange (e) {
     this.setData({
-      year: e.detail.value,
       [`formData.year`]: e.detail.value
+    })
+  },
+  tapDialogButton (e) {
+    this.setData({
+      dialogShow: false
     })
   }
 })
---
+++ miniprogram/pages/addpage/validate.js
@@ -0,0 +1,127 @@
+// 字段名称对应
+const fieldName = {
+  hospitalName: '医院名称',
+  department: '科室',
+  isOlympus: '品牌名称',
+  olympusModel: 'Olympus品牌型号',
+  laparNum: '腔镜数量',
+  brand: '其它品牌名称',
+  model: '型号',
+  year: '购买年份',
+  bedNum: '科室床位数',
+  income: '科室收入',
+  operationNum: '科室总手术量',
+  percent: '腔镜手术占比'
+}
+
+const fieldType = {
+  hospitalName: 'string',
+  department: 'string',
+  isOlympus: 'number',
+  olympusModel: 'string',
+  laparNum: 'number',
+  brand: 'string',
+  model: 'string',
+  year: 'number',
+  bedNum: 'number',
+  income: 'number',
+  operationNum: 'number',
+  percent: 'number'
+}
+
+const TEXT_MAX_LEN = 100
+const NUM_MAX_LEN = 20
+/**
+ * 验证数据
+ */
+function validate (formData) {
+  if (isEmpty(formData.isOlympus)) {
+    notify(`请选择${fieldName['isOlympus']}`)
+    return false
+  }
+  let checkItem = ['hospitalName', 'department', 'laparNum', 'brand', 'olympusModel']
+  // 为其它的时候
+  if (formData.isOlympus == 1) {
+    checkItem = ['hospitalName', 'department', 'laparNum', 'brand', 'model', 'year', 'bedNum', 'income', 'operationNum', 'percent']
+  }
+  let isok = true
+  for (let i = 0, le = checkItem.length; i < le; i++) {
+    let limit = fieldType[checkItem[i]] === 'string' ? TEXT_MAX_LEN : NUM_MAX_LEN
+    if (!textValidate(formData, checkItem[i], limit)) {
+      isok = false
+      break
+    }
+    if (fieldType[checkItem[i]] === 'number' && !isNumber(formData[checkItem[i]])) {
+      notify(`${fieldName[checkItem[i]]}只能输入数字`)
+      isok = false
+      break
+    }
+  }
+  if (!isok) {
+    return false
+  }
+  if (formData.isOlympus == 1) {
+    let percent = parseFloat(formData.percent)
+    if (percent < 0 || percent > 100) {
+      notify(`${fieldName['percent']}合法值为0-100`)
+      return false
+    }
+  }
+  return true
+}
+
+function textValidate (formData, filed, len) {
+  if (isEmpty(formData[filed])) {
+    notify(`请输入${fieldName[filed]}`)
+    return false
+  }
+  if (isGtLength(formData[filed], len)) {
+    notify(`${fieldName[filed]}超出长度限制`)
+    return false
+  }
+  return true
+}
+
+function notify (text) {
+  wx.showToast({
+    title: text,
+    icon: 'none',
+    duration: 2000
+  })
+}
+/**
+ * 判断是否为空
+ */
+function isEmpty (value) {
+  if (value === undefined || value === null || value === "") {
+    return true
+  }
+  if (value instanceof Array && value.length === 0) {
+    return true
+  }
+  if (typeof value === 'object' && JSON.stringify(value) == "{}") {
+    return true
+  }
+  return false
+}
+
+/**
+ * 校验长度，是否超过len
+ */
+function isGtLength (value, len) {
+  if (typeof value === 'object') {
+    return false
+  }
+  let length = 0
+  if (!isEmpty(value)) {
+    length = value.toString().length
+  }
+  return length > len
+}
+
+function isNumber (value) {
+  let re = /^[0-9]+.?[0-9]*$/
+  return re.test(value)
+}
+
+module.exports.validate = validate 
